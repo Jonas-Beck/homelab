cd97176a3607819b4a2da29b60d5fc7f1dcb2491
Jonas Beck <dev@jonasbeck.dk>
2025-12-23 12:50:13 +0100

    feat(yamllint): add githook instead of yq e inside kubeconform script
    Signed-off-by: Jonas Beck <dev@jonasbeck.dk>

--- mise-tasks/kubeconform.sh
+++ mise-tasks/kubeconform.sh
@@ -29,13 +29,6 @@ echo "INFO - Downloading Flux OpenAPI schemas"
 mkdir -p /tmp/flux-crd-schemas/master-standalone-strict
 curl -sL https://github.com/fluxcd/flux2/releases/latest/download/crd-schemas.tar.gz | tar zxf - -C /tmp/flux-crd-schemas/master-standalone-strict
 
-# TODO: This step can be optimized using hk pre-commit to only validate changed yaml files
-find . -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file; do
-	echo "INFO - Validating $file"
-	yq e 'true' "$file" >/dev/null
-done
-
-# TODO: Look into moving both these checks into seperate tasks / hk hooks.
 echo "INFO - Validating clusters"
 find ./clusters -maxdepth 2 -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file; do
 	kubeconform "${kubeconform_flags[@]}" "${kubeconform_config[@]}" "${file}"
